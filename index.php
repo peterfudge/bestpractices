47b44b43ecbcebf852f3ec20f3e7eed029e46983
Peter Fudge <pete.fudge@gmail.com>
2015-06-28 22:47:22 -0400

    set up dev mode / production switch

--- index.php
+++ index.php
@@ -53,7 +53,19 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+    $server = $_SERVER['HTTP_HOST'];
+    switch ($_SERVER['HTTP_HOST']) {
+        case 'locahost':
+            $env = 'development';
+         break;
+
+        default :
+            $env = 'production';
+        break;
+    }
+
+	//define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+    define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : $env);
 
 /*
  *---------------------------------------------------------------
